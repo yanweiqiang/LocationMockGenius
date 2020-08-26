b3e65927ff4a91167a4871882a91af718ca81c22
yanweiqiang04496 <yanweiqiang04496@hellobike.com>
2020-08-26 20:05:19 +0800

    Add navigation mock page

---
+++ app/src/main/java/com/yan/locationmockgenius/route/NavMockActivity.kt
@@ -0,0 +1,12 @@
+package com.yan.locationmockgenius.route
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.yan.locationmockgenius.R
+
+class NavMockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_mock)
+    }
+}
